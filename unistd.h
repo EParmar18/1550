e0f29123d6fdcb83e0a9410818888fb1984f37a2
Eshan Parmar <eshan.parmar@gmail.com>
2020-09-23 22:49:45 -0400

    Changed unistd.h to  remove sys from new defines for methods

--- unistd.h
+++ unistd.h
@@ -330,11 +330,11 @@
 #define __NR_timerfd		322
 #define __NR_eventfd		323
 #define __NR_fallocate		324
-#define __NR_sys_cs1550_create  325
-#define __NR_sys_cs1550_open  326
-#define __NR_sys_cs1550_down  327
-#define __NR_sys_cs1550_up  328
-#define __NR_sys_cs1550_close   329
+#define __NR_cs1550_create  325
+#define __NR_cs1550_open  326
+#define __NR_cs1550_down  327
+#define __NR_cs1550_up  328
+#define __NR_cs1550_close   329
 
 // add new syscall numbers here
 
