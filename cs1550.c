359c2eac81a7b53645859538e6c5cbf8bf821bd4
Eshan Parmar <eshan.parmar@gmail.com>
2020-09-23 22:48:26 -0400

    Finished cs1550_open method

--- cs1550.c
+++ cs1550.c
@@ -1,6 +1,6 @@
 #include <linux/cs1550.h>
 
-DEFINE_SPINLOCK(lock);
+DEFINE_SPINLOCK(sem_lock);
 struct cs1550_node semList = {0, NULL};
 long semIdentifier = 1000;
 
@@ -13,7 +13,16 @@ asmlinkage long sys_cs1550_create(int value, char name[32], char key[32]){
 }
 /* This syscall opens an already created semaphore by providing the semaphore name and the correct key. The function returns the identifier of the opened semaphore if the key matches the stored key or -1 otherwise. */
 asmlinkage long sys_cs1550_open(char name[32], char key[32]){
-  return 0;
+  struct cs1550_sem *semaphore;
+  spin_lock(&sem_lock);
+  semaphore = list_find_name_key(semList, name, key);
+  spin_unlock(&sem_lock);
+
+  if(temp != NULL)
+  {
+    return temp->sem_id;
+  }
+  return -1;
 }
 /* This syscall implements the down operation on an already opened semaphore using the semaphore identifier obtained from a previous call to sys_cs1550_create or sys_cs1550_open. The function returns 0 when successful or -1 otherwise (e.g., if the semaphore id is invalid or if the queue is full). Please check the lecture slides for the pseudo-code of the down operation. */
 asmlinkage long sys_cs1550_down(long sem_id){
@@ -38,7 +47,39 @@ asmlinkage long sys_cs1550_down(long sem_id){
   sem->refcount--
   unlock(global_spinlock)
   */
-  spinlock(&sem_lock);
+  struct cs1550_sem *semaphore;
+  spin_lock(&sem_lock);
+  semaphore = list_find_name_key(semList, name, key);
+  spin_unlock(&sem_lock);
+  semList->value -= 1;
+
+  if(semList->value < 0)
+  {
+    struct cs1550_node* initialNode = (struct cs1550_node*)kmalloc(sizeof(struct cs1550_node), GFP_ATOMIC);
+    initalNode->task = current;
+    initialNode->next = NULL;
+
+    if(semList->head == NULL)
+    {
+      semList->head = initialNode;
+    }
+    else
+    {
+      semList->tail->next = initialNode;
+    }
+
+    semList->tail = initialNode;
+    set_current_state(TASK_INTERRUPTIBLE);
+    spin_unlock(&sem_lock);
+    schedule();
+  }
+  else
+  {
+    spin_unlock(&sem_lock);
+  }
+  
+    
+  }
 
   return 0;
 }
@@ -48,7 +89,34 @@ asmlinkage long sys_cs1550_up(long sem_id){
   // wake_up_process(sleeping_task);
 
  // Psuedo Code 
+  spinlock(&sem_lock);
+  semList->value += 1;
 
+  if(semList->value <= 0)
+  {
+    struct task_struct* task;
+    struct cs1550_node* initialNode = semList->head;
+    if(initialNode != NULL)
+    {
+      process = initialNode->process;
+
+      if(initialNode == semList->tail)
+      {
+        semList->head = NULL;
+        semList->tail = NULL;
+      }
+      else
+      {
+        semList->head = initialNode->next;
+      }
+      wake_up_process(process);
+      
+    }
+    kfree(initialNode);
+  }
+  spin_unlock(&sem_lock);
+
+  return 0;
 
   return 0;
 }
--- unistd.h
+++ unistd.h
@@ -330,15 +330,18 @@
 #define __NR_timerfd		322
 #define __NR_eventfd		323
 #define __NR_fallocate		324
-#define __NR_sys_cs1550_down  325
-#define __NR_sys_cs1550_up  326
+#define __NR_sys_cs1550_create  325
+#define __NR_sys_cs1550_open  326
+#define __NR_sys_cs1550_down  327
+#define __NR_sys_cs1550_up  328
+#define __NR_sys_cs1550_close   329
 
 // add new syscall numbers here
 
 #ifdef __KERNEL__
 
 // change this number as well
-#define NR_syscalls 327
+#define NR_syscalls 330
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
