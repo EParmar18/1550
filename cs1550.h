09ef4b105a49fe899cec4522bfcffab849f7b007
Eshan Parmar <eshan.parmar@gmail.com>
2020-09-25 02:21:50 -0400

    Working cs1550.h file!

--- cs1550.h
+++ cs1550.h
@@ -13,31 +13,31 @@ struct cs1550_sem
    char name[32];
    //Some FIFO queue of your devising0
    struct cs1550_queue *semaphores;
-}
+};
 
 // Global semaphore list
 struct list_node{
    struct cs1550_sem sem;
    struct list_node *next;
-}
+};
 
 struct sem_list{
    struct list_node *head;
    struct list_node *tail;
-}
+};
 
 // Node for the semaphore queues
 struct queue_node{
    struct cs1550_node* next;
    struct task_struct* process;
-}
+};
 
 // Semaphore queue
 struct cs1550_queue{
    int count;
    struct queue_node *front;
    struct queue_node *rear;
-}
+};
 
 //Add to this file any other struct definitions that you may need
 
